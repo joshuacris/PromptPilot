2a8155f6c7853558e5479436358d50c88a243241
prithviseran <prithisha33@gmail.com>
2025-03-22 14:17:01 -0400

    rebase

--- chrome-extension-final/popup.tsx
+++ chrome-extension-final/popup.tsx
@@ -3,19 +3,11 @@ import { useState } from "react"
 function IndexPopup() {
   const [data, setData] = useState("")
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setData(e.target.value)
-  }
-
-  const handleSave = () => {
-    chrome.runtime.sendMessage({type: "input-change", value: data}, (response) => {
-      console.log("Message sent to background script")
-    })
-  }
-
   return (
     <div
-      style={{ padding: 16}}>
+      style={{
+        padding: 16
+      }}>
       <h2>
         Welcome to your{" "}
         <a href="https://www.plasmo.com" target="_blank">
@@ -23,11 +15,12 @@ function IndexPopup() {
         </a>{" "}
         Extension!
       </h2>
-      <input value ={data} onChange={handleChange} />
-      <button onClick={handleSave}> Save Input </button>
+      <input onChange={(e) => setData(e.target.value)} value={data} />
       <a href="https://docs.plasmo.com" target="_blank">
         View Docs
       </a>
     </div>
   )
 }
+
+export default IndexPopup
