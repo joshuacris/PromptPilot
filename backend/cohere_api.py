925a5d9a8919e4210b5e2c5c66f6490524720142
prithviseran <prithisha33@gmail.com>
2025-03-22 13:01:36 -0400

    flask endpoint

--- backend/cohere_api.py
+++ backend/cohere_api.py
@@ -1,36 +1,65 @@
 import cohere
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
 
-CO_API_KEY = "iV47bx7aPOKOfHF9LZzN7ie5HFvwRSxMOt8OwJTK" # get this from https://dashboard.cohere.com/
+load_dotenv()
+
+CO_API_KEY = os.environ.get("CO_API_KEY")# get this from https://dashboard.cohere.com/
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS to allow requests from your React app
+#CORS(app)  # Enable CORS to allow requests from your React app
+co = cohere.ClientV2(CO_API_KEY)
+PROMPT_RUBRIK = '''
+
+Given a prompt, can you please improve it so that it follows the Prompt Rubric:
+
+1. First, break the prompt into sequential steps: Eg. First [do something], then [do something]. Finally, [do something].
+
+2. Then, provide necessary context or background information. Eg. Report card. Which report card? For elementary students in Brazil?
+
+3. Afterwards, assign a role to the AI model. Eg. Imagine you are a teacher and I am a student. Please explain Newton’s Law.
+
+4. After this, specify the tone (Please use constructive tone, related to roles: whose perspective
+should the response be written from and who is the audience.). If possible, use action words to be more specific.
+
+5. If there are no examples, add an example when applicable.
+
+6. Explicitly say format of the response expected (bullet points, tables, code, etc).
+
+7. Include this at the end of the prompt: “Please do not assume anything, ask me any
+clarification questions if need be.
+
+8. Finally, please do not apply it to this prompt. Memorize the Prompt Rubric for future prompts.
+'''
 
 
 def get_cohere_response(role, in_message):
-    co = cohere.ClientV2(CO_API_KEY)
 
     response = co.chat(model="command-a-03-2025", messages=[{"role": role, "content": in_message}])
 
     return response
 
 
-@app.route('/api/submit', methods=['POST'])
+@app.route('/improve_prompt', methods=['POST'])
 def get_cohere_improved_prompt():
+
+    print("\nWorks!\n")
+
     content = request.json
 
     user_prompt = content["received"]["text"]
 
-    print('Received data:', content)
+    co.chat(model="command-a-03-2025", messages=[{"role": "user", "content": PROMPT_RUBRIK}])
 
-    return jsonify({'status': 'success', 'received': content}), 201
+    prompt = user_prompt + 'Improve this prompt using the Prompt Rubric, with the response being a string of the new prompt only.'
 
+    response = co.chat(model="command-a-03-2025", messages=[{"role": "user", "content": prompt}])
+
+    return jsonify({'status': 'success', 'improved_prompt': response}), 201
 
 
 if __name__ == "__main__":
-    co = cohere.ClientV2(CO_API_KEY)
 
-    response = co.chat(model="command-a-03-2025", messages=[{"role": "user", "content": "tell me about university of toronto"}])
-
-    print(response)
+    app.run(debug=True)
